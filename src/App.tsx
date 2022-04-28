dedcb8089a400b893bf57fb3b0cc9bd2155bbba3
ponpokoringo <81539080+ponpokoringo@users.noreply.github.com>
2022-04-28 15:19:40 +0900

    フォーム作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div>
+        <h2>Todo list with Typescript</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            type="text"
+            onChange={(e) => handleChange(e)}
+            className="inputText"
+          />
+          <input type="submit" value="作成" className="submitButton" />
+        </form>
+      </div>
     </div>
   );
 }
